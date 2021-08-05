66bac3dad2ef7e76aaf646270932ac2dfe8fd66f
ArshWalker <arshwalker7@gmail.com>
2021-08-05 06:39:51 +0530

    Footer.js Added

---
+++ components/Footer.js
@@ -0,0 +1,52 @@
+function Footer() {
+  return (
+    <div className="font-mono">
+      <hr className="w-[369px] md:w-[1348px] border-gray-800" />
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32  py-14 bg-gray-100 text-gray-600">
+        <div className="space-y-4 text-s text-gray-800 ">
+          <h5 className="font-bold">ABOUT</h5>
+          <p>How Airbnb works</p>
+          <p>Newsroom</p>
+          <p>Investors</p>
+          <p>Airbnb Plus</p>
+          <p>Airbnb Luxe</p>
+        </div>
+        <div className="space-y-4 text-s text-gray-800 ">
+          <h5 className="font-bold">COMMUNITY</h5>
+          <p>Acessibility</p>
+          <p>This is ❌ official site</p>
+          <p>This is clone</p>
+          <p>Support this</p>
+          <p>Walker Join</p>
+        </div>
+        <div className="space-y-4 text-s text-gray-800 ">
+          <h5 className="font-bold">HOST</h5>
+          <p>Host your home</p>
+          <p>Host an Online Experience</p>
+          <p>Host an Experience</p>
+          <p>Responsible hosting</p>
+          <p>Resource Centre</p>
+        </div>
+        <div className="space-y-4 text-s text-gray-800 ">
+          <h5 className="font-bold">SUPPORT</h5>
+          <p>Our COVID-19 Response</p>
+          <p>Help Centre</p>
+          <p>Cancellation options</p>
+          <p>Neighbourhood Support</p>
+          <p>Trust & Safety</p>
+        </div>
+      </div>
+      <div className="px-32 py-4 bg-gray-100 text-gray-600">
+        <hr className="w-full md:w-[1000px] border-gray-300" />
+        <div className="text-center ">
+          Made with ❤️ by{" "}
+          <a className="font-bold" href="http://arshwalker.netlify.app/">
+            Arshdeep.
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
