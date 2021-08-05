b1deaf772880fe8fe45639ec356dd3dea584b3af
ArshWalker <arshwalker7@gmail.com>
2021-08-05 06:40:08 +0530

    header updated

--- components/Header.js
+++ components/Header.js
@@ -8,9 +8,9 @@ import {
 } from "@heroicons/react/solid";
 function Header() {
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-1">
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-5">
       {/* div for left */}
-      <div className="relative flex items-center h-10 cursor-pointer">
+      <div className="relative flex items-center h-10 cursor-pointer ">
         <Image
           src="https://links.papareact.com/qd3"
           layout="fill"
