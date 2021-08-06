c29d58e6e5db19d6ce7146b214f103715f40051a
ArshWalker <arshwalker7@gmail.com>
2021-08-06 06:54:39 +0530

    frontend updated

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
+import DiscoverCard from "../components/DiscoverCard";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import LargeCard from "../components/LargeCard";
 import MediumCard from "../components/MediumCard";
 import SmallCard from "../components/SmallCard";
+import TryDeployCard from "../components/TryDeployCard";
 
-export default function Home({ exploreData, cardsData }) {
+export default function Home({ exploreData, cardsData, discoverData }) {
   return (
     // tailwind css
     <div className="">
@@ -21,7 +23,7 @@ export default function Home({ exploreData, cardsData }) {
       <Banner />
 
       {/* main */}
-      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+      <main className="max-w-7xl mx-auto px-8 sm:px-16 font-serif">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
 
@@ -53,6 +55,22 @@ export default function Home({ exploreData, cardsData }) {
           description="Wishlists created by Airbnb."
           buttonText="Get Inspired"
         />
+
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Discover things to do</h2>
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {discoverData?.map(({ img, title, desc }) => (
+              <DiscoverCard Key={img} img={img} title={title} desc={desc} />
+            ))}
+          </div>
+        </section>
+
+        <TryDeployCard
+          img="https://i.ibb.co/MNY1qzV/airbnb4.jpg"
+          title="Try hosting"
+          description="Earn extra income and unlock new oppurtunities by sharing your space."
+          buttonText="Learn more"
+        />
       </main>
 
       {/* footer */}
@@ -68,10 +86,14 @@ export async function getStaticProps() {
   const cardsData = await fetch("https://links.papareact.com/zp1").then((res) =>
     res.json()
   );
+  const discoverData = await fetch(
+    "http://www.json-generator.com/api/json/get/bVmPnJPglK?indent=2"
+  ).then((res) => res.json());
   return {
     props: {
       exploreData,
       cardsData,
+      discoverData,
     },
   };
 }
